8cfd76a562114ab0d33b9dca6d5d35d445add82c
ultraspicy <73217673+ultraspicy@users.noreply.github.com>
2023-05-17 00:00:48 -0700

    Add some solutions

--- leetcode_in_rust/src/main.rs
+++ leetcode_in_rust/src/main.rs
@@ -1,8 +1,10 @@
 use std::collections::HashMap;
 
 fn main() {
-    let ret = Solution::circular_game_losers(5, 2);
-    print_vec(ret.as_ref());
+    // let v = vec![vec![2,4,3,5], vec![5,4,9,3], vec![3,4,2,11]];
+    // println!("{}", Solution::max_moves(v));
+    let v: usize = 0;
+    println!("{}", v as i32 - 1);
 }
 
 fn print_vec(vec: &Vec<i32>) {
@@ -32,5 +34,55 @@ impl Solution {
         let losers = (1..=n).filter(|&f| !map.contains_key(&f)).collect();
 
         return losers;
+    }
+
+    pub fn does_valid_array_exist(derived: Vec<i32>) -> bool {
+        let mut a = 0;
+        for num in derived {
+            a = a ^ num;
+        }
+        return a == 0
+    }
+
+    pub fn max_moves(grid: Vec<Vec<i32>>) -> i32 {
+        let (m, n) = (grid.len(), grid[0].len());
+        let mut cache: Vec<Vec<usize>> = vec![vec![0; m]; n];
+        let mut ret: i32 = 0;
+        for i in 0..m {
+            ret = ret.max(Self::dfs(&grid, i, 0, 0, &mut cache) as i32);
+        }
+        return ret;
+    }
+
+    pub fn dfs(
+        grid: &Vec<Vec<i32>>,
+        last_row: usize,
+        last_col: usize,
+        steps: usize,
+        cache: &mut Vec<Vec<usize>>,
+    ) -> usize {
+        let (m, n) = (grid.len(), grid[0].len());
+        if last_col == n - 1 {
+            return n - 1;
+        }
+        if cache[last_row][last_col] != 0 {
+            return cache[last_row][last_col];
+        }
+
+        let (mut ret, val) = (0, grid[last_row][last_col]);
+        if last_row as i32 - 1 >= 0 && grid[last_row - 1][last_col + 1] > val {
+            ret = ret.max(Self::dfs(grid, last_row - 1, last_col + 1, steps + 1, cache));
+        }
+        if grid[last_row][last_col + 1] > val {
+            ret = ret.max(Self::dfs(grid, last_row, last_col + 1, steps + 1, cache));
+        }
+        if (last_row + 1) < m && grid[last_row + 1][last_col + 1] > val {
+            ret = ret.max(Self::dfs(grid, last_row + 1, last_col + 1, steps + 1, cache));
+        }
+        if ret == 0 {
+            ret = steps;
+        }
+        cache[last_row][last_col] = ret;
+        return ret;
     }
 }
