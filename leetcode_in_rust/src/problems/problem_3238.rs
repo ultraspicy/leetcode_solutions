67a599d4a96069fe49d9bddcf8d59664583f6f4d
ultraspicy <element.no.88@gmail.com>
2024-08-27 21:09:26 -0700

    fix: add rust solution for problem 3238

--- leetcode_in_rust/src/problems/problem_3238.rs
+++ leetcode_in_rust/src/problems/problem_3238.rs
@@ -3,19 +3,31 @@ use super::Solution;
 
 use std::{collections::HashMap, hash::Hash};
 
+// key learnings:
+//   entry() method returns an Entry enum, which represents a slot in the HashMap with its status (occupied or vacant)
+//   or_insert() method returns a mutable reference of V 
+//   iter() yeilds (&K, &V), when matching the pattern of (&k, &v) This pattern dereferences both references.|(k, v)|:
+//   This pattern keeps the references as they are.
+//     k is a reference to the key.
+//     v is a reference to the value.
+
+
+//   map.iter_mut(): Returns an iterator over mutable references to the values.
+//   map.into_iter(): Consumes the map and returns an iterator over owned key-value pairs. 
+
 impl Solution {
     pub fn winning_player_count(n: i32, picks: Vec<Vec<i32>>) -> i32 {
         let mut map:HashMap<i32, HashMap<i32, i32>> = HashMap::new();
         for pick in picks {
             let color_frequency = map.entry(pick[0]).or_insert(HashMap::new());
-            let _ = *color_frequency.entry(pick[1]).or_insert(0) + 1; 
+            *color_frequency.entry(pick[1]).or_insert(0) += 1; 
         }
 
         let win = map.iter().
             filter(
-                |(&k, v)| v.values().max().map_or(false, |&max| max > k + 1)).
+                |(&k, v)| v.values().max().map_or(false, |&max| max > k)).
             count() as i32;
 
-        n - win
+        win
     } 
 }  
--- src/Solution.java
+++ src/Solution.java
@@ -6,46 +6,9 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
+        // solution.minimumSemesters(3, new int[][]{{1,3},{2,3}});
         System.out.println("");
     }
 
-    public void wallsAndGates(int[][] rooms) {
-        // figure out the gates
-        List<int[]> gates = new ArrayList<>();
-        int m = rooms.length, n = rooms[0].length;
-        for(int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (rooms[i][j] == 0) {
-                    gates.add(new int[]{i, j});
-                }
-            }
-        }
 
-        // for each gate, BFS and update the min distance to the gate
-        int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        for(int[] gate: gates) {
-            // bfs for a single gate
-            Queue<int[]> q = new LinkedList<>();
-            q.add(gate);
-            boolean[][] visited = new boolean[m][n];
-            int distance = 1;
-            while (!q.isEmpty()) {
-                int size = q.size();
-                while(size-- > 0) {
-                    int[] head = q.poll();
-                    for(int[] direction : directions) {
-                        int newX = head[0] + direction[0], newY = head[1] + direction[1];
-                        if(newX >= 0 && newX < m && newY >= 0 && newY < n && rooms[newX][newY] != 0 && rooms[newX][newY] != -1 && !visited[newX][newY]) {
-                            rooms[newX][newY] = Math.min(distance, rooms[newX][newY]);
-                            q.add(new int[]{newX, newY});
-                            visited[newX][newY] = true;
-                        }
-                    }
-                }
-                distance++;
-            }
-        }
-        // traverse the rooms to update the isolated rooms
-        
-    }
 }
