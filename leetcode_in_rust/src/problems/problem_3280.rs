8ffc21b86ffffbfd3d5292ed0c35779d5ce8bd27
ultraspicy <element.no.88@gmail.com>
2024-09-17 23:27:11 -0700

    feat: Add problem_3280.rs

--- leetcode_in_rust/src/problems/problem_3280.rs
+++ leetcode_in_rust/src/problems/problem_3280.rs
@@ -5,4 +5,16 @@ impl Solution {
     // pub fn method_name(params) -> return_type {
     //     unimplemented!()
     // }
+    pub fn convert_date_to_binary(date: String) -> String {
+
+        date.split('-').map(|single_date| match Self::str_to_binary(single_date){
+            Ok(binary_date) => binary_date,
+            Err(_error) => String::from("0000"),
+        }).collect::<Vec<String>>().join("-")
+    }
+
+    fn str_to_binary(s: &str) -> Result<String, std::num::ParseIntError> {
+        let number = s.parse::<u32>()?;
+        Ok(format!("{:b}", number))
+    }
 }
