3e45dc61a670367fa5b816274b8e97b8528f82da
ultraspicy <element.no.88@gmail.com>
2024-09-18 21:26:25 -0700

    feat: Add problem_3270.rs

--- leetcode_in_rust/src/problems/problem_3270.rs
+++ leetcode_in_rust/src/problems/problem_3270.rs
@@ -5,4 +5,19 @@ impl Solution {
     // pub fn method_name(params) -> return_type {
     //     unimplemented!()
     // }
+    pub fn generate_key(num1: i32, num2: i32, num3: i32) -> i32 {
+        let str1 = format!("{:04}", num1);
+        let str2 = format!("{:04}", num2);
+        let str3 = format!("{:04}", num3);
+
+        let mut ret = String::new();
+        for i in 0..4 {
+            let max_char = str1.chars().nth(i).unwrap().min(str2.chars().nth(i).unwrap().min(str3.chars().nth(i).unwrap()));
+            ret.push(max_char);
+        }
+        match ret.parse::<i32>() {
+            Ok(num) => num,
+            Err(_) => 0,
+        }
+    }
 }
