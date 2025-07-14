d35340d9045424d5a2430dbc7d1cc4984077abda
Jianfeng Guo <element.no.88@gmail.com>
2025-07-14 14:55:04 -0700

    [chore] minor code refactor

--- leetcode_in_rust/src/problems/problem_3594.rs
+++ leetcode_in_rust/src/problems/problem_3594.rs
@@ -1,4 +1,4 @@
-use std::{cmp::Reverse, collections::{BinaryHeap, HashSet}, hash::{Hash, Hasher}};
+use std::{cmp::Reverse, collections::{BinaryHeap, HashSet}};
 use itertools::Itertools;
 
 use super::Solution;
@@ -19,7 +19,7 @@ impl Solution {
         if k == 1 && n > 1 {
             return -1.0
         }
-    
+
         // use last n bit as a bit mask
         let bit_mask: u32 = 0;
         // state is defined as (time to reach this state from init state, bit_mask, boat position, current stage)
@@ -35,18 +35,18 @@ impl Solution {
         let mut heap = BinaryHeap::new();
         heap.push(Reverse(init_state));
         let mut visited = HashSet::new();
-        
-        while !heap.is_empty() {     
+
+        while !heap.is_empty() {
             if let Some(Reverse((accumulted_time, current_mask, position, stage))) = heap.pop() {
                 if  current_mask == final_mask {
                     return accumulted_time.0;
-                } 
+                }
                 if !visited.insert((current_mask, position, stage)) {
                     continue;
                 }
                 println!("{:?}", (accumulted_time, current_mask, position, stage));
 
-                // forward path 
+                // forward path
                 if position == 0 {
                     let people_this_trip =  k.min(n - current_mask.count_ones() as i32);
                     let zero_position = (0..n as usize).filter(|i| {
@@ -93,8 +93,8 @@ impl Solution {
                             let new_position = 0;
                             let new_stage = (stage + (next_min_time.floor() as i32 % m) as usize) % mul.len();
                             heap.push(Reverse((OrderedFloat(new_time), new_state, new_position, new_stage)));
-                        });      
-                }               
+                        });
+                }
             }
         }
         -1 as f64
@@ -110,29 +110,29 @@ mod test {
     fn unit_test() {
         let ret = Solution::min_time_3594(1, 1, 2, vec![5], vec![1.0,1.3]);
         assert_eq!(ret, 5.0)
-    }   
+    }
 
     #[test]
     fn unit_test1() {
         let ret = Solution::min_time_3594(3, 2, 3, vec![2, 5, 8], vec![1.0,1.5,0.75]);
         assert_eq!(ret, 14.50)
-    } 
+    }
 
     #[test]
     fn unit_test2() {
         let ret = Solution::min_time_3594(3, 2, 3, vec![32, 13, 94], vec![1.33,1.41,1.38]);
         assert_eq!(ret, 188.08000)
-    } 
+    }
 
     #[test]
     fn unit_test3() {
         let ret = Solution::min_time_3594(3, 5, 5, vec![83, 2, 53], vec![1.98,0.75,1.28,0.57,1.88]);
         assert_eq!(ret, 161.14000)
-    } 
+    }
 
     // #[test]
     // fn unit_test4() {
     //     let ret = Solution::min_time_3594(3, 2, 4, vec![57, 80, 46], vec![1.37,1.81,0.52,1.66]);
     //     assert_eq!(ret, 240.53000)
-    // } 
+    // }
 }
--- leetcode_in_rust/src/problems/problem_3598.rs
+++ leetcode_in_rust/src/problems/problem_3598.rs
@@ -15,34 +15,34 @@ impl Solution {
         for i in 0..words.len() - 1 {
             let prefix_of_next = Self::common_prefix_len(&words[i], &words[i + 1]);
             Self::cascade(
-                i, 
-                i + 1, 
-                prefix_of_next, 
-                longest_prefix, 
-                second_longest, 
+                i,
+                i + 1,
+                prefix_of_next,
+                longest_prefix,
+                second_longest,
                 third_longest,
             );
         }
 
-        println!("{:?}", longest_prefix); 
+        println!("{:?}", longest_prefix);
         println!("{:?}", second_longest);
         println!("{:?}", third_longest);
 
         let mut ret = vec![];
         for i in 0..words.len() {
             let mut prefix_with_jump = 0;
-            let mut prefix_without_jump = 0;
-            if longest_prefix.1 != i && longest_prefix.2 != i {
-                prefix_without_jump = longest_prefix.0 
+
+            let prefix_without_jump = if longest_prefix.1 != i && longest_prefix.2 != i {
+                longest_prefix.0
             } else if second_longest.1 != i && second_longest.2 != i {
-                prefix_without_jump = second_longest.0 
+                second_longest.0
             } else {
-                prefix_without_jump = third_longest.0 
-            }
+                third_longest.0
+            };
             if i != 0  && i != words.len() - 1{
                 prefix_with_jump =  Self::common_prefix_len(&words[i - 1], &words[i + 1]);
             }
-            
+
             ret.push(prefix_with_jump.max(prefix_without_jump) as i32);
         }
 
@@ -52,14 +52,14 @@ impl Solution {
     fn common_prefix_len(s1: &str, s2: &str) -> usize {
         s1.chars().zip(s2.chars())
             .take_while(|&(c1, c2)| c1 == c2)
-            .count() 
+            .count()
     }
 
     fn cascade (
-        start_index: usize, 
+        start_index: usize,
         end_index: usize,
         prefix_len: usize,
-        longest_prefix: &mut (usize, usize, usize), 
+        longest_prefix: &mut (usize, usize, usize),
         second_longest: &mut (usize, usize, usize),
         third_longest: &mut (usize, usize, usize),
     ) {
@@ -73,7 +73,7 @@ impl Solution {
         } else if prefix_len >= third_longest.0 {
             *third_longest = (prefix_len, start_index, end_index);
         }
-    } 
+    }
 }
 
 #[cfg(test)]
